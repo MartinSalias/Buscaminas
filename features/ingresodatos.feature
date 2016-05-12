a757323e8e8e791756328ee3faac45450542c76a
Eric Valera Miller <eric@elericuz.com>
2016-05-12 15:45:53 -0500

    ingreso de valores

---
+++ features/ingresodatos.feature
@@ -0,0 +1,6 @@
+Feature: Ingreso de Datos
+Scenario: se debe poder ingresar datos y presionar Pinchar
+	Given El jugador ingresa al juego
+	When Ingresar 2,3 como posicion 
+	 And presionar "Pinchar"
+	Then Debe verse una carita feliz ":)"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,3 +1,17 @@
 Given(/^El jugador ingresa al juego$/) do
 	visit '/'
 end
+
+When(/^Ingresar (\d+),(\d+) como posicion$/) do |arg1, arg2|
+	visit '/'
+	fill_in("x", :with => arg1)
+	fill_in("y", :with => arg2)
+end	
+
+When(/^presionar "(.*?)"$/) do |pinchar|
+	click_button(pinchar)
+end
+
+Then(/^Debe verse una carita feliz "(.*?)"$/) do |arg1|
+end
+
