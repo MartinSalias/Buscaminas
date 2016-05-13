d083db82722f8ec898a7d75d2f43058193bfc5bc
Kleer LA <kleer@kleer-sandbox.(none)>
2016-05-13 14:35:01 -0300

    variable global

--- app.rb
+++ app.rb
@@ -2,26 +2,26 @@ require 'sinatra'
 require_relative './lib/tablero.rb'
 
 get '/' do
+	@@tablero = Tablero.new 
 	erb :home
 end
-
 
 post '/pinchaaar' do
 	x = params[x]
 	y = params[y]
-	tablero = Tablero.new
-	tablero.pinchar x, y
+	
+	@@tablero.pinchar x, y
 	@mensaje = "jojoj"
 	erb :resultado
 end
 
 get '/testhome' do
-	@tablero = params[:tablero]
+	@@tablero = params[:tablero]
 	erb :testhome
 end
 
 post '/pinchar' do
-	tablero = Tablero.new
-	@mensaje = tablero.pinchar params[:x].to_i, params[:y].to_i
+	
+	@mensaje = @@tablero.pinchar params[:x].to_i, params[:y].to_i
 	erb :resultado
 end
